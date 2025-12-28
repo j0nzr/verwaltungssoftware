0091f11836fa8aa0ae8b5767c91914cb9ee040a1
Jonas Zeiser <jonas@zeiser-sohn.de>
2025-12-28 21:34:09 +0100

    Trasnlation for database headers to real names

---
+++ src/libraries/databaseHeaders.ts
@@ -0,0 +1,12 @@
+export const mandanten = {
+    mandantName: "Mandanten-Name",
+    id: "ID",
+    zusatz: "Zusatz",
+    adresszeile1: "Adresse",
+    adresszeile2: "Adresszeile 2",
+    plz: "PLZ",
+    ort: "Ort",
+    verwaltungsart: "Verwaltungsart",
+    wirtschaftsjahrBeginn: "Wirtschaftsjahr-Beginn",
+    wirtschaftsjahrEnde: "Wirtschaftsjahr-Ende"
+}
